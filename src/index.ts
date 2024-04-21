c8e5461e8e67f926481c644eae651d8a019749a5
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-04-21 13:43:35 -0500

    KV example

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
 type Bindings = Record<string, unknown> & CloudflareBindings;
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const count = parseInt((await c.env.MY_KV_NAMESPACE.get("count")) || "0");
+  await c.env.MY_KV_NAMESPACE.put("count", String(count + 1));
 
-export default app
+  return c.json({ count });
+});
+
+export default app;
